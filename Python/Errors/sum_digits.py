b75a8bae5af538e61f97a5a01c19a3c57eafdcbb
prajna-spec <64818027+prajna-spec@users.noreply.github.com>
2021-10-20 02:38:56 +0530

    Update sum_digits.py
    Corrected the name of function in the recursive function call

--- Python/Errors/sum_digits.py
+++ Python/Errors/sum_digits.py
@@ -1,15 +1,14 @@
-
-# Sum of digits (recursive)
-
-# Write a recursive function
-# that returns the sum of the digits of a given integer.
-# Read input as specified in the question.
-# Print output as specified in the question.
-
-def dsum(n):
+# Recursive Python3 program to
+# find sum of digits of a number
+ 
+# Function to check sum of
+# digit using recursion
+def sum_of_digit( n ):
     if n == 0:
         return 0
     return (n % 10 + sum_of_digit(int(n / 10)))
-
-n = int(input())
-print(dsum(n))
+ 
+# Driven code to check above
+num = int(input())
+result = sum_of_digit(num)
+print("Sum of digits in",num,"is", result)
